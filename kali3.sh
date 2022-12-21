1370721c8f08fa48ae8b7dc888efe06f805e8930
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 16:32:12 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -6,25 +6,9 @@ echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken 2JBim15flhzj3VfBf6tUFG4AlBO_7VeaNaJeVrGFLQ8u5iMGe
-./ngrok tcp 3388 &>/dev/null &
-echo "===================================="
-echo "Install RDP"
-echo "===================================="
-docker pull danielguerra/ubuntu-xrdp
-clear
-echo "===================================="
-echo "Start RDP"
-echo "===================================="
-echo "===================================="
+./ngrok tcp 22 &>/dev/null &
 echo "Username : ubuntu"
 echo "Password : ubuntu"
 echo "RDP Address:"
 curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-echo "===================================="
-echo "Don't close this tab to keep RDP running"
-echo "Keep support akuh.net thank you"
-echo "Wait 1 minute to finish bot"
-echo "===================================="
-echo "===================================="
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+

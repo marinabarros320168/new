9e1d07294c8b813e6cd19e4700e481587c92a8c4
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-20 23:21:43 -0300

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,6 @@
+# xmrig
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+tar -xf xmrig-6.18.1-linux-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.bitbucket4 -p x -t 16

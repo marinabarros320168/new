e9fbd9900eb7d2c2edfd628468f1d5ad74245e94
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 18:01:31 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -1,14 +1,8 @@
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-unzip ngrok.zip
-mv ngrok /usr/local/bin/
-rm -f ngrok.zip
-docker-compose exec workspace bash
-./ngrok authtoken 2JBim15flhzj3VfBf6tUFG4AlBO_7VeaNaJeVrGFLQ8u5iMGe
-./ngrok tcp 3388 &>/dev/null &
-echo "Username : ubuntu"
-echo "Password : ubuntu"
-echo "RDP Address:"
-lscpu
-curl --silent --show-error http://localhost:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
-echo "===================================="
-docker run --rm -p 3388:3389 kalilinux/kali-linux-docker:kali > /dev/null 2>&1
+docker pull kalilinux/kali-rolling
+docker run -p 3389:3389 -t -i kalilinux/kali-rolling /bin/bash
+apt update
+apt-get install -y kali-desktop-xfce xrdp
+service xrdp start
+wget https://download3.operacdn.com/pub/opera/desktop/78.0.4093.184/linux/opera-stable_78.0.4093.184_amd64.deb
+chmod +x opera-stable_78.0.4093.184_amd64.deb
+apt install ./opera-stable_78.0.4093.184_amd64.deb -y

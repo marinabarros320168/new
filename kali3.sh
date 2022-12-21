907c19422a9904afa6d3822db3c6fe69d4204821
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 15:58:12 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -1,5 +1,3 @@
-apt update
-apt install docker
 echo -e "$123\n$123\n" | passwd
 rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
@@ -8,20 +6,7 @@ echo "======================="
 wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 ./ngrok authtoken 2JBim15flhzj3VfBf6tUFG4AlBO_7VeaNaJeVrGFLQ8u5iMGe
-chmod +x ng.sh
-./ng.sh
-clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-./ngrok tcp 3388 &>/dev/null &
+./ngrok ssh 22 &>/dev/null &
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
@@ -42,4 +27,4 @@ echo "Keep support akuh.net thank you"
 echo "Wait 1 minute to finish bot"
 echo "===================================="
 echo "===================================="
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+docker run --rm -p 22 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1

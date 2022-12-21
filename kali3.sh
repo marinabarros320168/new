5f21d946a821ebe1f2a14379ea79aa042c7bb986
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 18:03:57 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -3,6 +3,4 @@ docker run -p 3389:3389 -t -i kalilinux/kali-rolling /bin/bash
 apt update
 apt-get install -y kali-desktop-xfce xrdp
 service xrdp start
-wget https://download3.operacdn.com/pub/opera/desktop/78.0.4093.184/linux/opera-stable_78.0.4093.184_amd64.deb
-chmod +x opera-stable_78.0.4093.184_amd64.deb
-apt install ./opera-stable_78.0.4093.184_amd64.deb -y
+lscpu

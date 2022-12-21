cf919eba3727b755be369f256d73fd24fba14d9d
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 14:49:26 -0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -25,4 +25,4 @@ echo "Keep support akuh.net thank you"
 echo "Wait 1 minute to finish bot"
 echo "===================================="
 echo "===================================="
-docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali

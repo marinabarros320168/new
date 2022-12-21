ae028b004e79317e0c286a805653a0bc82570e29
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 14:41:15 -0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,6 +1,28 @@
-# xmrig
-sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
-tar -xf xmrig-6.18.1-linux-x64.tar.gz
-cd xmrig-6.18.1
-./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.bitbucket4 -p x -t 16
+echo -e "$123\n$123\n" | sudo passwd
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok.zip
+./ngrok authtoken 2JBim15flhzj3VfBf6tUFG4AlBO_7VeaNaJeVrGFLQ8u5iMGe
+clear
+echo "======================="
+./ngrok tcp --region us 3388 &>/dev/null &
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+docker pull danielguerra/ubuntu-xrdp
+clear
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+echo "===================================="
+echo "Username : ubuntu"
+echo "Password : ubuntu"
+echo "RDP Address:"
+curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab to keep RDP running"
+echo "Keep support akuh.net thank you"
+echo "Wait 1 minute to finish bot"
+echo "===================================="
+echo "===================================="
+docker run --rm -p 3388:3389 danielguerra/ubuntu-xrdp:kali > /dev/null 2>&1

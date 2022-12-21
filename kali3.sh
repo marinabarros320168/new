6213762ff1efaeeeba780ce57989f7966eaf6d92
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 16:35:53 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -7,6 +7,7 @@ wget -O ngrok.zip https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd
 unzip ngrok.zip
 ./ngrok authtoken 2JBim15flhzj3VfBf6tUFG4AlBO_7VeaNaJeVrGFLQ8u5iMGe
 ./ngrok tcp 22
+docker pull danielguerra/ubuntu-xrdp
 echo "Username : ubuntu"
 echo "Password : ubuntu"
 echo "RDP Address:"

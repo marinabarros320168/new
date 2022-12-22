e8b1b4daf3db0d0fa18fb27893435f6d5d660fce
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2022-12-21 23:19:22 -0300

    Update kali3.sh

--- kali3.sh
+++ kali3.sh
@@ -1,12 +1,3 @@
-echo "docker 1"
 docker pull kalilinux/kali-rolling
-echo "docker 2"
-docker run -p 3389:3389 -t -i kalilinux/kali-rolling /bin/bash
-echo "docker 3"
-apt update
-echo "docker 4"
-apt-get install -y kali-desktop-xfce xrdp
-echo "docker 5"
-service xrdp start
-echo "docker 6"
+docker run -ti kalilinux/kali-rolling /bin/bash
 lscpu

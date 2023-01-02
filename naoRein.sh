bccea7bc888b1530240dba8277e5a37e445b2367
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-01 23:22:03 -0300

    Create naoRein.sh

---
+++ naoRein.sh
@@ -0,0 +1,4 @@
+@echo off
+cd /d "%~dp0"
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Nao-Reiniciado-01-01-2 -p x -t 8
+pause 

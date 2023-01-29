23c42d441eae9cd1bb9bc4095e65899a45eeacf3
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-29 10:58:42 -0300

    Update srb.sh

--- srb.sh
+++ srb.sh
@@ -1,5 +1,5 @@
-# srbminer
+#!/bin/sh
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.9/SRBMiner-Multi-0-6-9-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-6-9-Linux.tar.xz
 cd SRBMiner-Multi-0-6-9
-./start-mining-epiccash-and-monero.sh
+./SRBMiner-MULTI --disable-gpu --multi-algorithm-job-mode 3 --algorithm randomepic;randomx --pool epic.hashrate.to:4000;monero.herominers.com:10190 --wallet epic-username-here;monero-wallet-here

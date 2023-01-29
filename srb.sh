cd3c190ab06730bd94b2c6bb6da338d9a5d3872e
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-29 19:57:53 -0300

    Update srb.sh

--- srb.sh
+++ srb.sh
@@ -1,6 +1,11 @@
-#!/bin/sh
-sudo apt update
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.9/SRBMiner-Multi-0-6-9-Linux.tar.xz
-tar -xf SRBMiner-Multi-0-6-9-Linux.tar.xz
-cd SRBMiner-Multi-0-6-9
-./SRBMiner-MULTI --disable-gpu --multi-algorithm-job-mode 3 --algorithm randomepic;randomx --pool epic.hashrate.to:4000;monero.herominers.com:10190 --wallet epic-username-here;monero-wallet-here
+apt-get update
+apt-get install build-essential
+apt-get install automake autoconf pkg-config libcurl4-openssl-dev
+apt-get install libjansson-dev libssl-dev libgmp-dev make g++ git
+mkdir /downloads
+cd /downloads
+git clone https://github.com/virtual-coin/cpuminer-multi
+cd cpuminer-multi/
+./build.sh
+make install
+cpuminer -a cryptonight -o stratum+tcp://rx.unmineable.com:443 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.User -p x

